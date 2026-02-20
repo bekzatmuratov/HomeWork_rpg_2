fefa3f6d7fce7bf4e335317ec4b7d8dd783437d5
Bekzat Muratov <bekzat.muratov@narxoz.kz>
2026-02-20 22:58:00 +0500

    Add IceComponentFactory (Abstract Factory)

---
+++ src/com/narxoz/rpg/factory/ice/IceComponentFactory.java
@@ -0,0 +1,34 @@
+package com.narxoz.rpg.factory.ice;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.combat.ice.FrostBreath;
+import com.narxoz.rpg.combat.ice.IceShield;
+import com.narxoz.rpg.factory.EnemyComponentFactory;
+import com.narxoz.rpg.loot.LootTable;
+import com.narxoz.rpg.loot.ice.IceLootTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Concrete Abstract Factory for Ice-themed enemies.
+ */
+
+public class IceComponentFactory implements EnemyComponentFactory {
+    @Override
+    public List<Ability> createAbilities() {
+        List<Ability> abilities = new ArrayList<>();
+        abilities.add(new FrostBreath());
+        abilities.add(new IceShield());
+        return abilities;
+    }
+
+    @Override
+    public LootTable createLootTable() {
+        return new IceLootTable();
+    }
+
+    @Override
+    public String createAIBehavior() {
+        return "DEFENSIVE";
+}
